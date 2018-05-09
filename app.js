bff2f0004197dc023060315a499704c40c8d9d59
shenjie <985184097@qq.com>
2018-05-09 23:13:31 +0800

    1

--- app.js
+++ app.js
@@ -11,13 +11,32 @@ app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/public')
 
 })
+//    io.on(‘connection’,function(socket));//监听客户端连接,回调函数会传递本次连接的socket
+//
+//    io.sockets.emit(‘String’,data);//给所有客户端广播消息
+//
+//    io.sockets.socket(socketid).emit(‘String’, data);//给指定的客户端发送消息
+//
+//    socket.on(‘String’,function(data));//监听客户端发送的信息
+//
+//    socket.emit(‘String’, data);//给该socket的客户端发送消息
 
 io.on('connection',(socket)=>{
-    console.log('connect')
+    console.log('connection')
+    // console.log(socket)
+    //socket.emit('hello','系统：欢迎新用户加入'); //向单一用户发消息
+    io.emit('hello','系统：欢迎新用户加入');//向所有用户发消息
+    socket.on('sendmsg',(data)=>{
+        console.log('sendmsg')
+    // console.log(socket)
+    //socket.emit('hello','系统：欢迎新用户加入'); //向单一用户发消息
+     io.emit('msg',data);//向所有用户发消息
+    })
+
 })
 
 app.get('/wsaddr',(req, res) =>{
-    // 建立ws连接
+
     res.send('root')
 
 })
