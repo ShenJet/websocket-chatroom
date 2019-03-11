e33ed7094d76dd901fd89e8028a8c3a6d7d0c826
shenjie <985184097@qq.com>
2019-03-11 12:36:32 +0800

    1

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/public')
 
 })
-//    io.on(‘connection’,function(socket));//监听客户端连接,回调函数会传递本次连接的socket
-//
-//    io.sockets.emit(‘String’,data);//给所有客户端广播消息
-//
-//    io.sockets.socket(socketid).emit(‘String’, data);//给指定的客户端发送消息
-//
-//    socket.on(‘String’,function(data));//监听客户端发送的信息
-//
-//    socket.emit(‘String’, data);//给该socket的客户端发送消息
 
 io.on('connection',(socket)=>{
     let userlist = {}
@@ -47,9 +38,17 @@ io.on('connection',(socket)=>{
         io.emit('msg',{username:userlist[socket.id],data,socketid:socket.id});//向所有用户发消息
     })
 
+    // 接收文件
+    socket.on('voiceupload',(data)=>{
+        console.log('fileupload')
+        // console.log('data:')
+        // console.log(data)
+
+    //socket.emit('hello','系统：欢迎新用户加入'); //向单一用户发消息
+        io.emit('voice',{username:userlist[socket.id],data,socketid:socket.id});//向所有用户发消息
+    })
+
 })
-
-
 
 const server = http.listen('3000',()=>{
     console.log('listen @3000 now')
