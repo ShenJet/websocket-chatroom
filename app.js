0cfdd407eed867cb1850e4c64c4374493620ded4
ShenJet <985184097@qq.com>
2019-04-01 17:23:36 +0800

    1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: ShenJie 
  * @Date: 2018-05-10 11:26:45 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-03-21 23:58:32
+ * @Last Modified time: 2019年4月1日17:17:20
  */
 const express = require('express')
 
@@ -13,11 +13,7 @@ const io = require('socket.io')(http)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/',(req, res) =>{
-
-    
-
     res.sendFile(__dirname + '/public')
-
 })
 
 io.on('connection',(socket)=>{
@@ -46,15 +42,13 @@ io.on('connection',(socket)=>{
         console.log("客户端发来指令：");
         console.log(data);
         
-        io.emit("command_to_client", data)
+        socket.broadcast.emit("command_to_client", data)
     })
     
-
-
     socket.on('username',(data)=>{
         userlist[socket.id] = data
         console.log(userlist)
-        io.emit('hello',`通知：${data}已加入同步课堂`);//向所有用户发消息
+        socket.broadcast.emit('hello',`通知：${data}已加入同步课堂`);//向所有用户发消息
     })
 
     //socket.emit('hello','系统：欢迎新用户加入'); //向单一用户发消息
